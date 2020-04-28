f933d4ea0b0c639fdabf9a1646dc7880489b819a
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-28 12:58:07 +0530

    let and const.

--- es6.js
+++ es6.js
@@ -15,3 +15,23 @@ console.log('sum56Arrow', sum56Arrow);
 const sumArrow2 = (n1, n2) => n1 + n2;
 const sum56Arrow2 = sumArrow2(12, 6);
 console.log('sum56Arrow2', sum56Arrow2);
+
+// CONST & LET
+// NEVER USE var
+// ALWAYS USE const
+// USE let WHERE YOU CANNOT USE const
+
+const name = 'Adarsh';
+console.log('name using const', name);
+// name = 'Jane';
+
+let username = 'Adarsh';
+console.log('username using let', username);
+username = 'Jane';
+console.log('username using let', username);
+
+var nameOfUser = 'Adarsh';
+console.log('nameOfUser using var', nameOfUser);
+nameOfUser = 'Jane';
+console.log('nameOfUser using var', nameOfUser);
+var nameOfUser = 'Kevin';
